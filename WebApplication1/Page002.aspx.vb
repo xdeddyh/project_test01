55ce16da2d63c94c4ef8f5030be035ece50222be
xdeddyh <xdeddyh@gmail.com>
2015-07-14 16:29:40 +0700

    ok akan dimasukan git, setelah ditarik ke local

--- WebApplication1/Page002.aspx.vb
+++ WebApplication1/Page002.aspx.vb
@@ -3,6 +3,10 @@
 
     Protected Sub Page_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
 
+        'ini adalah coding yang ditambahkan setelah diclone
+        Dim blnKonfirmasi As Boolean
+
+        blnKonfirmasi = False
     End Sub
 
 End Class
