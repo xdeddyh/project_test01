7ab0a7395b2b0db2644c762a3273036b70792562
xkartinar <xkartinar@gmail.com>
2015-07-16 09:25:24 +0700

    ini dari komputer kart

---
+++ WebApplication1/page003_kar.aspx.vb
@@ -0,0 +1,12 @@
+﻿Public Class page003_kar
+    Inherits System.Web.UI.Page
+
+    Protected Sub Page_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
+        ' ini dimasukan dari pengguna lainnya
+
+        ' ok akan ditambahkan
+        Dim strTmp As String
+        strTmp = ""
+    End Sub
+
+End Class
